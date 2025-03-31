8ff5df4e599aac1e9722c5bdeb217fccb44b34e2
Le Duy Quan <leduyquan2574@gmail.com>
2025-03-31 15:22:04 +0700

    commit 31/3

---
+++ StudyOfDuyQuan/nodejs/src/cachcaidatthuvien.js
@@ -0,0 +1,18 @@
+// buoc 0: cai dat express: npm i --save-exact express@5.0.1                        
+// buoc 1: pm i --save-exact --save-dev typescript@5.7.3 @types/express@5.0.0 @types/node@18.17.0
+// typescript -> cai dat thu vien de dich typescript sang javascript
+// @types/express -> cai dat thu vien de goi y code type cho express
+// @types/node -> cai dat thu vien de goi y code type cho nodejs
+
+// buoc 2: npx tsc --init: tao file tsconfig.json -> de tao config cho typescript
+
+// buoc 3: tao file co tuoi ts
+
+// buoc 4: chay npx tsc: chay file ts sang js
+// buoc 5: chay node dist/ungdung.js chay file js
+
+// buoc 6: vao tsconfig.json -> chinh sua cac thuoc tinh -> chinh rootDir: src -> muc dich chay file ts trong src
+
+// buoc 7: vao package.json -> chinh sua cac thuoc tinh -> chinh scripts: 
+    // "start": "tsc && node dist/app.js",
+    // "dev": "tsc && node dist/app.js"
